600dd1f2735e8ace8cb9e876390fe1817cbdd52e
Aleks Rutins <aleks@rutins.com>
2022-10-25 18:06:21 -0400

    Start working on site info display

--- public/js/site-info.js
+++ public/js/site-info.js
@@ -1,7 +1,36 @@
 import { LitElement, html } from "./lit-core.min.js";
+import './components/loader.js';
 
 class AppView extends LitElement {
+    static properties = {
+        data: {},
+        error: {}
+    }
+    constructor() {
+        super();
+        this.data = null;
+        this.error = false;
+        const id = location.pathname.split('/').pop();
+        fetch(`/api/sites/${id}`)
+            .then(res => res.json())
+            .then(res => this.data = res)
+            .catch(e => {
+                console.log(e);
+                this.error = true;
+            });
+    }
     render() {
-        
+        return html`
+            ${
+            this.error
+                ? html`<span class="error">Error fetching site data</span>`
+                : this.data
+                    ? html`
+                        <h1>${this.data.site.domain}</h1>
+                    `
+                    : html`<ll-loader></ll-loader>`
+            }
+        `
     }
 }
+customElements.define('ll-app', AppView);
