1ff03e58b2cb201875302e49114b1c907659be40
Aleks Rutins <aleks@rutins.com>
2022-11-26 08:02:30 -0500

    Make sure the email is an email

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,8 +1,13 @@
 import { reactive } from 'vue'
 import * as cookie from '../util/cookie'
 
+function isEmail(str: string) {
+    return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+           .test(str)
+}
+
 export function isAuthenticated() {
-    return document.cookie.includes("userId=") && document.cookie.includes("userEmail=")
+    return cookie.has('userId') && cookie.has('userEmail') && isEmail(cookie.get('userEmail')!)
 }
 
 export function getEmail() {
