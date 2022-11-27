d4afa51bb1a3e1340e90bfaf9a84237c5737f23e
Aleks Rutins <aleks@rutins.com>
2022-11-27 11:13:10 -0500

    Actually send JSON data

--- frontend/public/simpleclient.js
+++ frontend/public/simpleclient.js
@@ -3,7 +3,10 @@ const url = new URL(import.meta.url)
 url.pathname = '/api/track'
 fetch(url, {
     method: 'POST',
-    body: {
+    body: JSON.stringify({
         'referrer': document.referrer
+    }),
+    headers: {
+        'Content-Type': 'application/json'
     }
 })
