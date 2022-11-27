f8b550de92a66272d54a87a359cba02ae0342819
Aleks Rutins <aleks@rutins.com>
2022-11-27 11:06:08 -0500

    Use a POST request

--- frontend/public/simpleclient.js
+++ frontend/public/simpleclient.js
@@ -2,6 +2,7 @@ console.log('Initializing Litelytics for ' + location.hostname)
 const url = new URL(import.meta.url)
 url.pathname = '/api/track'
 fetch(url, {
+    method: 'POST',
     body: {
         'referrer': document.referrer
     }
